2bf4a38c8b4c83958dec29ae0ad9db3841496d11
Max Razhnov <shahteryshka@gmail.com>
2018-02-22 00:27:46 +0300

    calculate love triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,26 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+	var n=0;
+	var isolatedIndexes=[];
+	var count=0;
+  //console.log(preferences.length);
+	while(n < preferences.length) {
+		if (isolatedIndexes.filter(function(index) {
+			return index===preferences[n];
+		}).length === 0) {
+		var a=preferences[n];
+		var b=preferences[a-1];
+		var c=preferences[b-1];
+		if(a===preferences[c-1] && n!==(a-1) && (a-1)!==(b-1)) {
+			count++; 
+			isolatedIndexes.push(a);
+			isolatedIndexes.push(b);
+			isolatedIndexes.push(c);
+      //console.log('isolatedIndexe ' + isolatedIndexes.toString())
+		}}
+	n++;
+}
+return count;
+}
+  
